d821d4f361d3e300d913e0393f176c401e9e3905
Lui <luiza.nd.veleiros@gmail.com>
2024-09-19 16:40:52 -0300

    js e contatos

--- perfilcamily-main/js/script.js
+++ perfilcamily-main/js/script.js
@@ -2,6 +2,9 @@ const toggleTheme = document.getElementById("toggleTheme");
 
 const rootHtml = document.documentElement;
 
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+
+const menulink = document.querySelectorAll(".menu-link");
 
 //função para alternar o tema
 
@@ -19,3 +22,12 @@ function changeTheme () {
 toggleTheme.addEventListener("click", changeTheme);
 
 //fim da função mudar o tema
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
